bf2d4e647811b2466a215bcaa339a0e71118e71f
Nate Bruggeman <njbruggeman@gmail.com>
2021-10-01 12:50:29 -0500

    looping through users with static data

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
-    state = {
-        id: 'id',
-        login: 'mojombo',
-        avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-        html_url: "https://github.com/mojombo"
-    }
-    render() {
-        return (
 
+    render() {
+        const { login, avatar_url, html_url } = this.props.user;
+        return (
             <div className= "card text-center">
-                <img src={this.state.avatar_url} className="round-img" style={{width: '60px'}} alt="avatar"/>
-                <h3>{this.state.login}</h3>
-                <div><a href={this.state.html_url} className="btn btn-dark btn-sm my-1">More</a>  </div>
+                <img src={avatar_url} className="round-img" style={{width: '60px'}} alt="avatar"/>
+                <h3>{login}</h3>
+                <div><a href={html_url} className="btn btn-dark btn-sm my-1">More</a>  </div>
                 <div></div>
             </div>
         )
