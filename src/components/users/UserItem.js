e76e578c1c0a358d72f45701257ced8182eb363b
Nate Bruggeman <njbruggeman@gmail.com>
2022-02-15 09:28:01 -0600

    refactored search bar to functional component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-class UserItem extends Component {
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        className="round-img"
+        style={{ width: "60px" }}
+        alt="avatar"
+      />
+      <h3>{login}</h3>
+      <div>
+        <Link to={`user/${login}`} className="btn btn-dark btn-sm my-1">
+          More
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-    render() {
-        const { login, avatar_url, html_url } = this.props.user;
-        return (
-            <div className= "card text-center">
-                <img src={avatar_url} className="round-img" style={{width: '60px'}} alt="avatar"/>
-                <h3>{login}</h3>
-                <div><a href={html_url} className="btn btn-dark btn-sm my-1">More</a>  </div>
-                <div></div>
-            </div>
-        )
-    }
-}
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
-export default UserItem
+export default UserItem;
